bf99e6db53b17bf1e5cd9f18f21aa42ae09686b4
William Mills <wmpmills@gmail.com>
2022-11-03 09:51:07 +0000

    Update desktop-restore.js

--- desktop-restore.js
+++ desktop-restore.js
@@ -1,10 +1,47 @@
+/********************************************************
+Copyright (c) 2022 Cisco and/or its affiliates.
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+               https://developer.cisco.com/docs/licenses
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+*********************************************************
+ * 
+ * Macro Author:      	William Mills
+ *                    	Technical Solutions Specialist 
+ *                    	wimills@cisco.com
+ *                    	Cisco Systems
+ * 
+ * Version: 1-0-0
+ * Released: 11/01/22
+ * 
+ * This is a simple Webex Devices Macro which shows your local desktop 
+ * after an answered call has closed it.
+ * 
+ * Full Readme and source code availabel on Github:
+ * https://github.com/wxsd-sales/desktop-restore-macr
+ * 
+ ********************************************************/
 import xapi from 'xapi';
+
+/*********************************************************
+ * Configure the settings below
+**********************************************************/
 
 const config = {
   showAlerts: true,         // true = show alert when the preview has been restored
   name: 'Preview Restore',   // The title text shown on the alert
   layout: 'Prominent'       
 }
+
+/*********************************************************
+ * Below contains all the call event listeners
+**********************************************************/
 
 let callEvent = {}
 
